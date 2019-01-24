36b25c887ed24d05e0c307d2486d45832065d3be
Erin Toth <erintoth@gmail.com>
2019-01-24 22:27:13 +0000

    Switched tweeter function to store data on MongoDB

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,102 +1,91 @@
 $(() => {
-
-$( "#compose" ).click(function() {
-  $( ".new-tweet" ).slideToggle( "slow", function() {
-    // Animation complete.
+  $('#compose').click(() => {
+    $('.new-tweet').slideToggle('slow', () => {
+    });
+    $('#tweetText').focus();
   });
-  $( "#tweetText" ).focus();
 
-});
-
-function renderTweets(tweets) {
-  for (tweet of tweets) {
+  function renderTweets(tweets) {
+    for (tweet of tweets) {
     // createTweetElement(tweet);
-    let $tweet = createTweetElement(tweet);
-    $('#tweets-container').prepend($tweet);
+      const $tweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweet);
 
     // console.log($tweet);
-  }
+    }
 
   // var $tweet = createTweetElement(tweetData);
-};
+  }
 
 
-function createTweetElement(data) {
-  const userName = data.user.name;
-  const userAvatarRegular = data.user.avatars.regular;
-  const userHandle = data.user.handle;
-  const contentText = data.content.text;
-  const created_at = data.created_at;
+  function createTweetElement(data) {
+    const userName = data.user.name;
+    const userAvatarRegular = data.user.avatars.regular;
+    const userHandle = data.user.handle;
+    const contentText = data.content.text;
+    const created_at = data.created_at;
 
-  const articleElement = $("<article>").addClass("tweets");
+    const articleElement = $('<article>').addClass('tweets');
 
-  $("<header>").appendTo(articleElement)
-    .append($("<img>").attr('src', userAvatarRegular).addClass("user"))
-    .append($("<span>").addClass("name").text(userName))
-    .append($("<span>").addClass("handle").text(userHandle));
+    $('<header>').appendTo(articleElement)
+      .append($('<img>').attr('src', userAvatarRegular).addClass('user'))
+      .append($('<span>').addClass('name').text(userName))
+      .append($('<span>').addClass('handle').text(userHandle));
 
-  $("<section>").addClass("content").text(contentText).appendTo(articleElement);
+    $('<section>').addClass('content').text(contentText).appendTo(articleElement);
 
-  $("<footer>").text(created_at).appendTo(articleElement)
-    .append($("<span>").addClass("icons"));
-
-
-  return articleElement;
+    $('<footer>').text(created_at).appendTo(articleElement)
+      .append($('<span>').addClass('icons'));
 
 
-};
+    return articleElement;
+  }
 
 
-
- $("#new-tweet").on('submit', function(event) {
+  $('#new-tweet').on('submit', function (event) {
     event.preventDefault();
     const serialized = $(this).serialize();
-    $(".error").text("").slideToggle( "slow", function () {
+    $('.error').text('').slideToggle('slow', () => {
 
     });
     console.log(serialized.length);
     if (serialized.length <= 5) {
-     $(".error").text("You have to twit more!").slideToggle( "fast", function() {
+      $('.error').text('You have to twit more!').slideToggle('fast', () => {
 
-     });
-
+      });
     } else if (serialized.length > 5 && serialized.length <= 140) {
       $.ajax({
-          method: "POST",
-          url: "/tweets",
-          data: serialized
-        }).done(function() {
-          loadTweets();
-
-        });
+        method: 'POST',
+        url: '/tweets',
+        data: serialized,
+      }).done(() => {
+        loadTweets();
+      });
     } else if (serialized.length > 140) {
-      $(".error").text("Twit less, please!").slideToggle( "fast", function() {
+      $('.error').text('Twit less, please!').slideToggle('fast', () => {
 
-     });
-
+      });
     }
-
   });
 
-const allTweets = $("#tweets-container");
+  const allTweets = $('#tweets-container');
 
- function loadTweets() {
-  $.ajax({
-    method: "GET",
-    url: "/tweets"
-  }).done(function(tweets) {
-    allTweets.empty();
+  function loadTweets() {
+    $.ajax({
+      method: 'GET',
+      url: '/tweets',
+    }).done((tweets) => {
+      allTweets.empty();
 
-    renderTweets(tweets);
+      renderTweets(tweets);
       // allTweets.prepend(element);
-      })
-
-  };
+    });
+  }
 
 
   loadTweets();
 
-// renderTweets(data);
+  // renderTweets(data);
 
 // Test / driver code (temporary)
 // var $tweet = createTweetElement(tweetData);
@@ -109,4 +98,3 @@ const allTweets = $("#tweets-container");
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-
