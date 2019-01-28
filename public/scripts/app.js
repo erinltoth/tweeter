a40aeaebb7c72cdffe7f9dd66bbdf565d53cae09
Erin Toth <erintoth@gmail.com>
2019-01-28 17:19:19 +0000

    Fix bugs with error for posting >140 characters and < 5 characters. Add function to adapt time stamp to be more readable.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,7 +15,7 @@ $(() => {
     const userAvatarRegular = data.user.avatars.regular;
     const userHandle = data.user.handle;
     const contentText = data.content.text;
-    const created_at = data.created_at;
+    const created_at = timeSince(data.created_at);
 
     const articleElement = $('<article>').addClass('tweets');
 
@@ -58,10 +58,10 @@ $(() => {
     const serialized = $(this).serialize();
     $('.error').text('').slideUp('slow', () => {
     });
-    if (serialized.length <= 5) {
+    if ($('#tweetText').val().length <= 0) {
       $('.error').text('You have to twit something!').slideToggle('fast', () => {
       });
-    } else if (serialized.length > 5 && serialized.length <= 140) {
+    } else if ($('#tweetText').val().length > 0 && $('#tweetText').val().length < 141) {
       $.ajax({
         method: 'POST',
         url: '/tweets',
@@ -71,11 +71,42 @@ $(() => {
         $('#tweetText').val('');
         $('span.counter').text('140');
       });
-    } else if (serialized.length > 140) {
-      $('.error').text('Twit a little less, please!').slideUp('fast', () => {
+    } else if ($('#tweetText').val().length > 140) {
+      $('.error').text('Twit a little less, please!').slideToggle('fast', () => {
       });
     }
   });
+
+  // Function to change time from milliseconds to a more readable format adapted from 
+  // Moses Koledoye's function found at https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
+  const timeSince = function(createdTime) {
+    let date = new Date(createdTime);
+    let seconds = Math.floor((new Date() - date) / 1000);
+    if (seconds < 5){
+        return "just now";
+    } else if (seconds < 60){
+        return seconds + " seconds ago";
+    } else if (seconds < 3600) {
+        minutes = Math.floor(seconds/60)
+        if(minutes > 1)
+            return minutes + " minutes ago";
+        else
+            return "1 minute ago";
+    } else if (seconds < 86400) {
+        hours = Math.floor(seconds/3600)
+        if(hours > 1)
+            return hours + " hours ago";
+        else
+            return "1 hour ago";
+    }
+    else {
+        days = Math.floor(seconds/86400)
+        if(days > 1)
+            return days + " days ago";
+        else
+            return "1 day ago";
+    }
+}
 
 
   loadTweets();
